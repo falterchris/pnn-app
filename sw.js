0ee7a8e3068c1bf149fd3767bc249b54b6e13cd0
falterchris <christian.falter@cip-marketing.com>
2026-01-26 03:06:29 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+// sw.js - Der Hintergrund-Dienst
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: '/icon.png', // Pfad zu deinem Event-Logo
+        badge: '/badge.png',
+        vibrate: [200, 100, 200],
+        data: { url: data.url }
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+});
